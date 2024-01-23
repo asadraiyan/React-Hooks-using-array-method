95519fa83513afe7938bb8b5eecd582490e4a406
Asad Raiyan <asadraiyan001@gmail.com>
2024-01-23 11:16:12 +0530

    upadated

--- src/components/accordion/Accordion.js
+++ src/components/accordion/Accordion.js
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
 import "./Accord.css";
 import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
+
 import { questions } from "./Questions";
+
 const Accordion = () => {
-  const [show, setShow] = useState(false);
   const [data, setData] = useState(questions);
+
+  const handleToggle = (id) => {
+    setData((prevData) =>
+      prevData.map((item) =>
+        item.id === id ? { ...item, show: !item.show } : item
+      )
+    );
+  };
+
   return (
     <>
       <div className="main_container">
@@ -13,21 +24,22 @@ const Accordion = () => {
             <h2>FAQ</h2>
           </div>
           <div className="accordion_wrapper">
-            {data.map((item) => {
-              return (
-                <div className="accordion_block" key={item.id}>
-                  <div className="accord_box" onClick={() => setShow(!show)}>
-                    <h3>{item.question}</h3>
-                    <IoIosArrowDown />
-                  </div>
-                  {show && (
-                    <div className="content">
-                      <p>{item.answer}</p>
-                    </div>
-                  )}
+            {data.map((item) => (
+              <div className="accordion_block" key={item.id}>
+                <div
+                  className="accord_box"
+                  onClick={() => handleToggle(item.id)}
+                >
+                  <h3>{item.question}</h3>
+                  {item.show ? <IoIosArrowUp /> : <IoIosArrowDown />}
                 </div>
-              );
-            })}
+                {item.show && (
+                  <div className="content">
+                    <p>{item.answer}</p>
+                  </div>
+                )}
+              </div>
+            ))}
           </div>
         </div>
       </div>
